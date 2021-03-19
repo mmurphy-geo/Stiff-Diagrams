38ded28818dd9d680c17d90609a8743dcbb43c81
Michael Murphy <80005367+mmurphy-geo@users.noreply.github.com>
2021-03-18 17:01:23 -0700

    Expanded and refactored to class notation

--- meq_calcs.py
+++ meq_calcs.py
@@ -8,22 +8,30 @@ Created on Wed Mar 17 14:50:06 2021
 
 import pandas as pd
 
-data = '2020_Q3_stiff_data.xlsx'
-df1 = pd.read_excel(data, engine='openpyxl')
+class MeqCalc:
+    
+    def __init__(self, data):
+        self.data = data
+        self.df1 = pd.read_excel(self.data, engine='openpyxl') # All data
+        self.df2 = self.df1[["LocCode", "Field_ID", "Sampled_Date-Time", "ChemName", "Conc_num"]] # Required columns
+        
+    def calc_meq(self):
+        
+        # Expected analytes and sorting: ['Alkalinity (Bicarbonate)','Calcium','Chloride','Magnesium','Potassium','Sodium','Sulfate']
+        chems = sorted(self.df2.ChemName.unique().tolist())
+        ions = ["HCO3-", "Ca++", "Cl-", "Mg++", "Na+ + K+", "Na+ + K+", "SO4--"]
+        charges = [-1, 2, -1, 2, 1, 1, -2]
+        molar_masses = [61.0168, 40.078, 35.453, 24.305, 39.0983, 22.99, 96.06]
+        chem_zip = zip(chems, charges, molar_masses, ions)
+        
+        # Write mass charge to dataframe for conversion to meq/L
+        self.df2["mass_charge"] = 0
+        self.df2["meq/L"] = 0
+        self.df2["ion"] = ''
+        for chem, charge, mass, ion in chem_zip:
+            self.df2.loc[self.df2.ChemName==chem, 'mass_charge'] = mass / charge
+            self.df2.loc[self.df2.ChemName==chem, "meq/L"] = self.df2.Conc_num / self.df2.mass_charge
+            self.df2.loc[self.df2.ChemName==chem, "ion"] = ion
+        return self.df2
 
-df2 = df1[["LocCode", "Field_ID", "Sampled_Date-Time", "ChemName", "Conc_num"]]
-
-chems = sorted(df2.ChemName.unique().tolist())
-charges = [-1, 2, -1, 2, 1, 1, -2]
-molar_masses = [61.0168, 40.078, 35.453, 24.305, 39.0983, 22.99, 96.06]
-
-chem_zip = zip(chems, charges, molar_masses)
-# Write mass charge to dataframe for conversion to meq/L
-df2["mass_charge"] = 0
-df2["meq/L"] = 0
-for chem, charge, mass in chem_zip:
-    df2.loc[df2.ChemName==chem, 'mass_charge'] = mass / charge
-    df2.loc[df2.ChemName==chem, "meq/L"] = df2.Conc_num / df2.mass_charge
-
-#def calc_meq():
     
